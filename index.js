8a7fdce6bc371e8d6958b82054f579cc5274d4cc
Samitha Hewawasam <samithahewawasam@192.168.1.3>
2024-05-05 21:27:50 +0530

    node dns

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Resolver, lookup } = require("node:dns");
+const argv = require("minimist")(process.argv.slice(2));
+
+/**
+ * @param -d domain
+ */
+
+const domainName = argv["d"];
+
+/**
+ * @param -setdns custom dns set
+ */
+
+const dnsServer = argv["setdns"];
+
+/**
+ * @description check domain validity
+ */
+
+const reg = new RegExp("[^a-z0-9-.]", "i");
+const isDomainValid = domainName && !reg.test(domainName);
+
+/**
+ * @description lookup
+ */
+if (isDomainValid)
+  lookup(domainName, (err, address, family) => {
+    console.log("address: %j family: IPv%s", address, family);
+  });
+
+console.log(isDomainValid, argv);
+
+if (isDomainValid && dnsServer) {
+  const resolver = new Resolver();
+  resolver.setServers([dnsServer]);
+
+  resolver.resolve4(domainName, (err, address, family) => {
+    console.log("address: %j family: IPv%s", address, family);
+  });
+}
